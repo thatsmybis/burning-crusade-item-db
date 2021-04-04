47de3264de3165ce0f69a035cf31f54d3c0a0f6e
Joe Lynch <joelynch1@gmail.com>
2021-04-04 15:31:19 -0700

    Added short name to instances.

--- thatsmybis/insert_instances.sql
+++ thatsmybis/insert_instances.sql
@@ -1,14 +1,14 @@
 -- Create a list of 'instances' for end-game raiding.
 
-INSERT INTO `instances` (`name`, `slug`, `order`, `created_at`)
+INSERT INTO `instances` (`name`, `short_name`, `slug`, `order`, `created_at`)
 VALUES
-    ("Karazhan",             "karazhan",             9,  "2021-02-16 00:00:00"), -- 2562
-    ("Gruul\'s Lair",        "gruuls-lair",          10, "2021-02-16 00:00:00"), -- 3618
-    ("Magtheridon\'s Lair",  "magtheridons-lair",    11, "2021-02-16 00:00:00"), -- 3836
-    ("Serpentshrine Cavern", "serpentshrine-cavern", 12, "2021-02-16 00:00:00"), -- 3607
-    ("Hyjal Summit",         "hyjal-summit",         13, "2021-02-16 00:00:00"), -- 3606
-    ("Tempest Keep",         "tempest-keep",         14, "2021-02-16 00:00:00"), -- 3842
-    ("Black Temple",         "black-temple",         15, "2021-02-16 00:00:00"), -- 3959
-    ("Zul\'Aman",            "zulaman",              16, "2021-02-16 00:00:00"), -- 3805
-    ("Sunwell Plateau",      "sunwell-plateau",      17, "2021-02-16 00:00:00"), -- 4075
-    ("World Bosses",         "bc-world-bosses",      18, "2021-03-20 00:00:00");
+    ("Karazhan",             "Kara",       "karazhan",             9,  "2021-02-16 00:00:00"), -- 2562
+    ("Gruul\'s Lair",        "Gruul",      "gruuls-lair",          10, "2021-02-16 00:00:00"), -- 3618
+    ("Magtheridon\'s Lair",  "Mag",        "magtheridons-lair",    11, "2021-02-16 00:00:00"), -- 3836
+    ("Serpentshrine Cavern", "SSC",        "serpentshrine-cavern", 12, "2021-02-16 00:00:00"), -- 3607
+    ("Hyjal Summit",         "Hyjal",      "hyjal-summit",         13, "2021-02-16 00:00:00"), -- 3606
+    ("Tempest Keep",         "TK",         "tempest-keep",         14, "2021-02-16 00:00:00"), -- 3842
+    ("Black Temple",         "BT",         "black-temple",         15, "2021-02-16 00:00:00"), -- 3959
+    ("Zul\'Aman",            "ZA",         "zulaman",              16, "2021-02-16 00:00:00"), -- 3805
+    ("Sunwell Plateau",      "Sunwell",    "sunwell-plateau",      17, "2021-02-16 00:00:00"), -- 4075
+    ("World Bosses",         "World Boss", "bc-world-bosses",      18, "2021-03-20 00:00:00");
